8eaa8a58e10870cb0eec5013e8354f39a2705158
Alexandr Shevchenko <shao-sss@hotmail.com>
2022-02-21 15:13:25 +0200

    Fix windows script

--- lib/Windows/buildNativeLibs.sh
+++ lib/Windows/buildNativeLibs.sh
@@ -2,10 +2,10 @@ sudo apt-get update
 
 sudo apt-get install mingw-w64 mingw-w64-tools autoconf libtool libudev-dev make gettext git -y
 
-sh ./../buildLibUsb.sh x86_64-w64-mingw32 $1
-sh ./../buildLibgpg-error.sh x86_64-w64-mingw32 $1 '--enable-threads=windows'
-sh ./../buildLibgcrypt.sh x86_64-w64-mingw32 $1
-sh ./../buildIconv.sh x86_64-w64-mingw32 $1
-sh ./../buildLibmtp.sh x86_64-w64-mingw32 $1
+sh $1/buildLibUsb.sh x86_64-w64-mingw32 $2
+sh $1/buildLibgpg-error.sh x86_64-w64-mingw32 $2 '--enable-threads=windows'
+sh $1/buildLibgcrypt.sh x86_64-w64-mingw32 $2
+sh $1/buildIconv.sh x86_64-w64-mingw32 $2
+sh $1/buildLibmtp.sh x86_64-w64-mingw32 $2
 
 cd ..
