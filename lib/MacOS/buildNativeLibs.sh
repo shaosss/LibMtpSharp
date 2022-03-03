5d4574f047dcdbfcce018eab17d749c3c4d1306f
Alexandr Shevchenko <shao-sss@hotmail.com>
2022-03-03 13:15:54 +0200

    Fix brew commands

--- lib/MacOS/buildNativeLibs.sh
+++ lib/MacOS/buildNativeLibs.sh
@@ -1,6 +1,13 @@
 brew update
 
-brew install pkg-config autoconf libtool libudev-dev make gettext git
+brew install pkg-config
+brew install autoconf
+brew install libtool
+brew install libudev-dev
+brew install make
+brew install gettext
+brew install git
+brew install automake
 
 sh $1/buildLibUsb.sh x86_64-apple-darwin $2
 sh $1/buildLibgpg-error.sh x86_64-apple-darwin $2
