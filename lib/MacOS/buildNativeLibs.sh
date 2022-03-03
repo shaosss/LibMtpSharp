c981f448d4a45452380f49b1ec7fa07c1b4e14fe
Alexandr Shevchenko <shao-sss@hotmail.com>
2022-03-03 12:31:07 +0200

    Add test macOS packages build

---
+++ lib/MacOS/buildNativeLibs.sh
@@ -0,0 +1,10 @@
+brew update
+brew upgrade
+
+brew install pkg-config autoconf libtool libudev-dev make gettext git -y
+
+sh $1/buildLibUsb.sh x86_64-apple-darwin $2
+sh $1/buildLibgpg-error.sh x86_64-apple-darwin $2
+sh $1/buildLibgcrypt.sh x86_64-apple-darwin $2
+sh $1/buildIconv.sh x86_64-apple-darwin $2
+sh $1/buildLibmtp.sh x86_64-apple-darwin $2
