8676d389b1f6e78f1f448faaa57f2d977c1cf251
Alexandr Shevchenko <shao-sss@hotmail.com>
2022-03-10 16:04:13 +0200

    change the target branch for libmtp

--- lib/buildLibmtp.sh
+++ lib/buildLibmtp.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/shaosss/libmtp.git
 cd libmtp/
-git checkout  master-custom
+git checkout  master-libmtpsharp
 
 printf 'n\n' | PKG_CONFIG_LIBDIR=$2/lib/pkgconfig/ LDFLAGS=-L$2/lib CPPFLAGS=-I$2/include ./autogen.sh --host=$1 --prefix=$2
 make
